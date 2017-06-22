eb78c4d16821fe8728f78414c3625f3837510749
rtoews <rtoews@instance-web.c.kinetic-books-2015-94218.internal>
2017-06-22 22:20:13 +0000

    Add logout function.

--- fpp/Chapters/core/main/js/view.js
+++ fpp/Chapters/core/main/js/view.js
@@ -1352,7 +1352,8 @@
           overlayDown()
           break;
         case "logout":
-          overlayDown()
+          overlayDown();
+          logout();
           break;
         case "play":
           overlayDown()
@@ -1542,6 +1543,15 @@
       $(p.mShell).find(".pMenu").removeClass('pSelected');
       $(p.mShell).find(".pMenuWrap").slideUp();
     }
+    function logout() {
+      var logoutUrl = p.bookData.logout_link;
+      var loginPage = p.bookData.login_link;
+      $.get(logoutUrl)
+        .done((resp) => { console.log('Success'); window.location = loginPage;})
+        .fail((resp) => { console.log('Failure'); })
+      ;
+    }
+
     this.loadScreen = function (data) {
       if (data.type != "labPage") {
         playerbtnManager(data);
