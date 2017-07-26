13e74ecbec7ae06b3cda8f13e194722a5603d2ee
RickToews <rick.toews@gmail.com>
2017-07-26 18:51:49 +0000

    Bit of code refactor to allow Submit your answers to include unsaved exercise answers.

--- fpp/Chapters/core/main/js/labData.js
+++ fpp/Chapters/core/main/js/labData.js
@@ -1,3 +1,36 @@
+/*
+ * Notes on handling lab answer save:
+ * There are two entirely separate actions for saving lab answers:
+ * 1. Storage of answers between lab sessions, without submitting for the gradebook. The Save button handles this.
+ * 2. Submitting lab answers for the gradebook. The Submit your answers button handles this.
+ * The REST calls for these processes are different.
+ *
+ * If the Save button is clicked, only action 1 is performed.
+ * If the Submit your answers button is clicked, actions 1 and 2 are both performed for each answer.
+ * The functions called are:
+ * Button: Save
+ *   handleSaveButton: Get the 'ques_*' exercise identifier for the clicked button;
+ *     setLabAns: Receive 'ques_*' string. Extract the user input for the indicated exercise, and store it in the local lab.ans hash.
+ *     saveLabAnswerList: Perform the REST call (lab.setLink) to save labData for this assignment to the database. (Does not affect gradebook.)
+ *     
+ * Button: Submit your answers
+ *   handleSubmitButton: For the lab-submit button, call submitLabAnswers.
+ *     submitLabAnswers: For each lab exercise, setLabAns; if the exercise is answered (isAnswered returns true), call sendLabResponse.
+ *       setLabAns: See above, under handleSaveButton.
+ *       isAnswered: Check the exercise to see if its field contains data.
+ *       sendLabResponse: Perform the REST call (submitLink) to save the problem submision to the database. (Affects gradebook.)
+ *
+ * Button: Print your answers
+ *   handleSubmitButton: For the lab-print button, call printLabAnswers.
+ *
+ *
+ * Explanatory note on the Save button handler: 
+ *   Formerly, the Save button handler responded directly to the click event and
+ *   stored the exercise input in the lab.ans hash before calling saveUserAnswer
+ *   to perform the REST call. Because the need to store exercise input can 
+ *   arise in multiple contexts, the storage in the hash was separated from the 
+ *   button handler itself.
+ */
   var LabData = function () {
     var lab = {}
     var device = BrowserDetect.any();
@@ -56,7 +89,7 @@
         $(lab.mShell).find(".pLabSaveButton").off("mouseout", mouseout).on("mouseout", mouseout);
       }
       $(lab.mShell).find(".pLabSaveButton").off(mouseEvents.down).on(mouseEvents.down, mousedown);
-      $(lab.mShell).find(".pLabSaveButton").off(mouseEvents.up).on(mouseEvents.up, saveLabAnswer);
+      $(lab.mShell).find(".pLabSaveButton").off(mouseEvents.up).on(mouseEvents.up, handleSaveButton);
       $(lab.mShell).find(".pLabSubmitButton").off("click", handleSubmitButton).on("click", handleSubmitButton);
 
     }
@@ -131,7 +164,7 @@
       lab.ans["ques_" + i] = _val;
         }
       });
-      saveUserAnswer();
+      saveLabAnswerList();
       console.log(lab.ans);
     }
 
@@ -225,6 +258,24 @@
   }
 
 
+    function submitLabAnswers() {
+/*
+    console.log('Submit button clicked.', lab.problemIds);
+    var _ids = document.querySelectorAll('div.pLabSaveButton');
+console.log('submitLabAnswers data-ids', _ids);
+*/
+      var keys = Object.keys(lab.problemIds);
+  console.log('submitLabAnswers keys', keys);
+      keys.forEach((key) => {
+        setLabAns(key);
+        if (isAnswered(key)) {
+          var probInstId = lab.problemIds[key];
+          sendLabResponse(probInstId, lab.ans[key]);
+        }
+      });
+      saveLabAnswerList();
+    }
+
   function sendLabResponse(probInstId, answer) {
       console.log("user lab response");
       var _data = JSON.stringify({"studentResponse": answer });
@@ -258,44 +309,47 @@
       });
   }
 
-  function saveLabAnswer() {
-    $(".pLabSaveButton").removeClass('pHover').removeClass('pDown')
-    var _type = $(this).attr("data-type");
-    var _id = $(this).attr('data-id');
-    console.log(_id);
-    var _userInput, _val;
-    if (_type == "textarea") {
-      _userInput = "";
-      _userInput = $(this).parent().find('.pLabAnswerBox').val();
-    } else if (_type == "table") {
-      _userInput = [];
-      $(this).parent().find(".pLabAnswerInput").each(function () {
-        _val = $(this).val();
-        _val = _val.replace(/[$|$]/g, '~');
-        _userInput.push(_val);
-      })
-      _userInput = _userInput.join("$|$");
+    // Separate handling of Save button from actually getting the field content and saving it.
+    function handleSaveButton(e) {
+      $(".pLabSaveButton").removeClass('pHover').removeClass('pDown')
+      var target = e.target;
+      var quesNo = $(target).data('id');
+      setLabAns(quesNo);
+      saveLabAnswerList();
     }
 
-    lab.ans[_id] = _userInput;
-    saveUserAnswer();
+    // Accept the ques_x question identifier, use it to locate the input field(s).
+    function setLabAns(quesNo) {
+      var target = document.querySelectorAll('[data-id=' + quesNo + ']');
+      var type = $(target).data('type');
+      if (type === 'textarea') {
+        var userInput = $(target).parent().find('.pLabAnswerBox').val();
+      } else {
+        var items = [];
+        $(target).parent().find(".pLabAnswerInput").each(function (idx, item) {
+          var tmp = $(item).val();
+          tmp = tmp.replace(/[$|$]/g, '~');
+          items.push(tmp);
+        });
+        userInput = items.join('$|$');
+      }
+      lab.ans[quesNo] = userInput;
+    }
 
-  }
-
-        function saveUserAnswer() {
-        console.log("user response");
-      showLoader();       var _data = JSON.stringify({"lab_data": lab.ans});
-        //  var _data = {"lab_data": lab.ans};
-        console.log(_data);
-        var request = $.ajax({
-      url: lab.setLink,
+    function saveLabAnswerList() {
+      showLoader();
+      var _data = JSON.stringify({"lab_data": lab.ans});
+//  var _data = {"lab_data": lab.ans};
+      console.log(_data);
+      var request = $.ajax({
+        url: lab.setLink,
         xhrFields: {
-        withCredentials: true
+          withCredentials: true
         },
         headers: {"Content-Type": "application/json"},
         crossDomain: true,
-      data: _data,
-  method: "PUT",
+        data: _data,
+        method: "PUT",
         dataType: "json"
       });
 
@@ -303,7 +357,6 @@
         console.log("user data updated");
         hideLoader();
         request = null;
-
       });
 
       request.fail(function (jqXHR, textStatus) {
@@ -313,5 +366,4 @@
         alert("please login");
       });
     }
-
   }
